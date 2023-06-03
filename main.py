861f5a4008dc18cea0e70b389becafca36a50d1c
Pyro <84557296+PyroCalzone@users.noreply.github.com>
2023-06-03 04:50:54 -0500

    v1.0.1
    Changed how the program detects deaths. Looks for when the Pilot returns to the briefing room instead of when the game despawns the vehicle. This works more accurately when doing multi-crew vehicles

--- main.py
+++ main.py
@@ -34,11 +34,11 @@ import subprocess
 import uuid
 import vdf
 import winreg
-
+import re
+import time 
 
 def get_log_path(): # Returns the path to the log file
-    appdata_path = os.path.abspath(os.path.join(os.getenv('APPDATA'), os.pardir)) # This is the path to the appdata folder (C:\Users\USERNAME\AppData\)
-    return os.path.join(appdata_path, "LocalLow", "Boundless Dynamics, LLC", "VTOLVR", "player.log") 
+    return os.path.join(os.getenv('APPDATA'), os.pardir, "LocalLow", "Boundless Dynamics, LLC", "VTOLVR", "player.log") 
 
 def getDirectory(): # Returns the path to the radio folder
     steam_path = winreg.QueryValueEx(winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\WOW6432Node\Valve\Steam"), "InstallPath") # This grabs the steam installation folder from the registry
@@ -57,37 +57,65 @@ def getDirectory(): # Returns the path to the radio folder
     finalPath = os.path.join(appDrive, "steamapps", "common", "VTOL VR", "RadioMusic")
     return finalPath
 
-Directory = getDirectory()
-def renameFilesRandomly(): # This shuffles the songs in the radio folder
-    for filename in os.listdir(Directory):
+def renameFilesRandomly(directory): # This shuffles the songs in the radio folder
+    for filename in os.listdir(directory):
         if filename.endswith(".mp3"):
             if filename.startswith("randomSong"):
                 originalName = filename[17:-5]
             else:
                 originalName = filename[:-4]
-            os.rename(os.path.join(Directory, filename), os.path.join(Directory, "randomSong" + str(uuid.uuid4())[:5] + f" ({originalName})" +".mp3")) # This renames the file to randomSong + a random string + the original name + .mp3
-            continue
-        else:
-            continue
+            os.rename(os.path.join(directory, filename), os.path.join(directory, "randomSong" + str(uuid.uuid4())[:5] + f" ({originalName})" +".mp3")) # This renames the file to randomSong + a random string + the original name + .mp3
 
 
 if __name__ == "__main__": #This handles the log file, new lines and setting the pilot name
+    counter = 0
+    deaths = 0
+    Directory = getDirectory()
     pilot = None
+    numberOfLines = 0 #Number of lines at startup
+    currentLine = 0 #Number of lines processed
+    logFilePath = get_log_path()
+
+    #add a flag to see if we've reached the end of the log file on startup
+
+    with open(logFilePath, "r", encoding="utf-8", errors="ignore") as f:
+        numberOfLines = len([line for line in f.readlines() if line.strip()])
+    
 
     def on_new_line(line):
+        search = re.search(r'^MP Spawn \[\d+\] \((.+?) \((.+?)\)\).SetTeam\(Allied\)', line)
+        #print(line)
         global pilot
+        global counter
+        global deaths
         if "Set current pilot to " in line:
             pilot = line.split("Set current pilot to ")[1]
-        elif f"{pilot} unspawned vehicle" in line:
-            renameFilesRandomly()
+            print(f"Set pilot to {pilot}")
+        elif "Returning to briefing room." in line:
+            counter += 1
+            if counter == 2: # "Returning to briefing room." is printed twice when the pilot dies, wait for the second one
+                deaths += 1
+                counter = 0
+                if currentLine > numberOfLines:
+                    print(f"{pilot} died, shuffling songs. Death count: {deaths}")
+                    time.sleep(2.5)
+                    try:
+                        renameFilesRandomly(Directory)
+                    except PermissionError:
+                        print("PermissionError, trying again")
+                        time.sleep(2)
+                        renameFilesRandomly(Directory)
         elif "Stopping EndMission" in line:
-            renameFilesRandomly()
+            print(f"{pilot} left the lobby, shuffling songs")
+            if currentLine > numberOfLines: 
+                renameFilesRandomly(Directory)
+            deaths = 0
 
-    logFilePath = get_log_path()
     tail_process = subprocess.Popen(["powershell.exe", 'Get-Content ', f'"{logFilePath}"', '-Wait'], stdout=subprocess.PIPE) # open the log file and waits for new lines
 
     while True:
         line = tail_process.stdout.readline()
         line = line.decode("utf-8", errors='ignore').strip()
         if line:
+            currentLine += 1
             on_new_line(line)
